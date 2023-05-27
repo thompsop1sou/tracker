24800a2d95f2a0b60509e6627902478bcf20dae4
Peter Thompson <thompsop1@sou.edu>
2023-05-27 03:48:53 -0700

    Update date module (safer methods)

--- src/date.rs
+++ src/date.rs
@@ -15,28 +15,41 @@ impl fmt::Display for Date {
 }
 
 impl Date {
-    // Create a new Date with default values year: 2000, month: 1, day: 1
+    // Create a new Date with default values (year: 2000, month: 1, day: 1)
     pub fn new() -> Date {
         Date{year: 2000, month: 1, day: 1}
     }
 
     // Create a new Date with today's date
-    pub fn today() -> Date {
+    pub fn new_from_today() -> Date {
         let today = Local::now().date_naive();
-        Date::from_string(&today.format("%Y-%m-%d").to_string()).unwrap()
+        Date::new_from_string(&today.format("%Y-%m-%d").to_string()).unwrap()
     }
 
     // Create a new Date from integer arguments for year, month, and day
-    pub fn from_ints(year: u16, month: u16, day: u16) -> Result<Date, &'static str> {
+    pub fn new_from_ints(year: u16, month: u16, day: u16) -> Result<Date, &'static str> {
         let mut date = Date::new();
-        date.set_year(year)?;
-        date.set_month(month)?;
-        date.set_day(day)?;
+        date.set_from_ints(year, month, day)?;
         Ok(date)
     }
 
-    // Create a new Date from a string argument formatted like "year.month.day"
-    pub fn from_string(date_str: &str) -> Result<Date, &'static str> {
+    // Create a new Date from a string argument formatted like "year-month-day"
+    pub fn new_from_string(date_str: &str) -> Result<Date, &'static str> {
+        let mut date = Date::new();
+        date.set_from_string(date_str)?;
+        Ok(date)
+    }
+
+    // Set this Date from integer arguments for year, month, and day
+    pub fn set_from_ints(self: &mut Self, year: u16, month: u16, day: u16) -> Result<(), &'static str> {
+        self.set_year(year)?;
+        self.set_month(month)?;
+        self.set_day(day)?;
+        Ok(())
+    }
+
+    // Set this Date from a string argument formatted like "year-month-day"
+    pub fn set_from_string(self: &mut Self, date_str: &str) -> Result<(), &'static str> {
         let parts: Vec<&str> = date_str.split("-").collect();
         if parts.len() == 3 {
             let year: u16;
@@ -54,7 +67,7 @@ impl Date {
                 Ok(x) => day = x,
                 Err(_) => return Err("Date parse error: cannot parse day"),
             }
-            Date::from_ints(year, month, day)
+            self.set_from_ints(year, month, day)
         } else {
             Err("Date parse error: incorrect number of seperators")
         }
@@ -65,43 +78,44 @@ impl Date {
         format!("{}-{}-{}", self.year.to_string(),self.month.to_string(), self.day.to_string())
     }
 
-    pub fn set_year(self: &mut Self, year: u16) -> Result<(), &'static str> {
+    // Get a tuple representation of this Date
+    pub fn to_tuple(self: &Self) -> (u16, u16, u16) {
+        (self.year, self.month, self.day)
+    }
+
+    fn set_year(self: &mut Self, year: u16) -> Result<(), &'static str> {
         self.year = year;
         Ok(())
     }
 
-    pub fn get_year(self: &Self) -> u16 {
-        self.year
-    }
-
-    pub fn set_month(self: &mut Self, month: u16) -> Result<(), &'static str> {
+    fn set_month(self: &mut Self, month: u16) -> Result<(), &'static str> {
         if month < 1 {
-            Err("Set month error: month too small")
+            Err("Set date error: month too small")
         } else if month > 12 {
-            Err("Set month error: month too large")
+            Err("Set date error: month too large")
         } else {
             self.month = month;
             Ok(())
         }
     }
 
-    pub fn get_month(self: &Self) -> u16 {
-        self.month
-    }
-
-    pub fn set_day(self: &mut Self, day: u16) -> Result<(), &'static str> {
+    fn set_day(self: &mut Self, day: u16) -> Result<(), &'static str> {
+        let is_leap_year = self.year % 4 == 0 && !(self.year % 100 == 0 && !(self.year % 400 == 0));
+    
+        let days_in_month = match self.month {
+            4 | 6 | 9 | 11 =>  30,
+            2 => if is_leap_year {29} else {28},
+            _ => 31,
+        };
+    
         if day < 1 {
-            Err("Set day error: day too small")
-        } else if day > 31 {
-            Err("Set day error: day too large")
+            Err("Set date error: day too small")
+        } else if day > days_in_month {
+            Err("Set date error: day too large")
         } else {
             self.day = day;
             Ok(())
         }
-    }
-
-    pub fn get_day(self: &Self) -> u16 {
-        self.day
     }
 }
 
@@ -110,45 +124,66 @@ mod tests {
     use crate::date::Date;
 
     #[test]
-    fn from_string() {
-        assert_eq!(Date::from_string("2012-11-21"),
-                    Ok(Date::from_ints(2012, 11, 21).unwrap()));
-        assert_eq!(Date::from_string("2030-1"),
+    fn parsing() {
+        // Can be parsed
+        assert_eq!(Date::new_from_string("2012-11-21"),
+                    Ok(Date::new_from_ints(2012, 11, 21).unwrap()));
+        // Can't be parsed because of separators
+        assert_eq!(Date::new_from_string("2030-1"),
                     Err("Date parse error: incorrect number of seperators"));
-        assert_eq!(Date::from_string("20"),
+        assert_eq!(Date::new_from_string("20"),
                     Err("Date parse error: incorrect number of seperators"));
-        assert_eq!(Date::from_string("2030-1-3-4"),
+        assert_eq!(Date::new_from_string("2030-1-3-4"),
                     Err("Date parse error: incorrect number of seperators"));
-        assert_eq!(Date::from_string("e-1-4"),
+        // Can't be parsed because of non-integers
+        assert_eq!(Date::new_from_string("e-1-4"),
                     Err("Date parse error: cannot parse year"));
-        assert_eq!(Date::from_string("2021-e-4"),
+        assert_eq!(Date::new_from_string("2021-e-4"),
                     Err("Date parse error: cannot parse month"));
-        assert_eq!(Date::from_string("2021-1-e"),
+        assert_eq!(Date::new_from_string("2021-1-e"),
                     Err("Date parse error: cannot parse day"));
     }
 
     #[test]
-    fn to_string() {
-        assert_eq!(Date::new().to_string(),
-                    String::from("2000-1-1"));
-        assert_eq!(Date::from_ints(2023, 5, 27).unwrap().to_string(),
-                    String::from("2023-5-27"));
+    fn representing() {
+        let date1 = Date::new();
+        let date2 = Date::new_from_ints(2023, 5, 27).unwrap();
+        // Representing as string
+        assert_eq!(date1.to_string(), String::from("2000-1-1"));
+        assert_eq!(date2.to_string(), String::from("2023-5-27"));
+        // Representing as tuple
+        assert_eq!(date1.to_tuple(), (2000, 1, 1));
+        assert_eq!(date2.to_tuple(), (2023, 5, 27));
     }
 
     #[test]
-    fn setters() {
+    fn setting() {
         let mut date = Date::new();
-        assert_eq!(date.set_month(0), Err("Set month error: month too small"));
-        assert_eq!(date.get_month(), 1);
-        assert_eq!(date.set_month(13), Err("Set month error: month too large"));
-        assert_eq!(date.get_month(), 1);
-        assert_eq!(date.set_month(6), Ok(()));
-        assert_eq!(date.get_month(), 6);
-        assert_eq!(date.set_day(0), Err("Set day error: day too small"));
-        assert_eq!(date.get_day(), 1);
-        assert_eq!(date.set_day(32), Err("Set day error: day too large"));
-        assert_eq!(date.get_day(), 1);
-        assert_eq!(date.set_day(15), Ok(()));
-        assert_eq!(date.get_day(), 15);
+        // Setting month
+        assert_eq!(date.set_from_ints(2000, 1, 15), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 0, 15), Err("Set date error: month too small"));
+        assert_eq!(date.set_from_ints(2000, 12, 15), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 13, 15), Err("Set date error: month too large"));
+        // Setting day in January (31 days)
+        assert_eq!(date.set_from_ints(2000, 1, 1), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 1, 0), Err("Set date error: day too small"));
+        assert_eq!(date.set_from_ints(2000, 1, 31), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 1, 32), Err("Set date error: day too large"));
+        // Setting day in April (30 days)
+        assert_eq!(date.set_from_ints(2000, 4, 1), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 4, 0), Err("Set date error: day too small"));
+        assert_eq!(date.set_from_ints(2000, 4, 30), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 4, 31), Err("Set date error: day too large"));
+        // Setting day in February (normal: 28 days, leap year: 29 days)
+        assert_eq!(date.set_from_ints(2000, 2, 1), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 2, 0), Err("Set date error: day too small"));
+        assert_eq!(date.set_from_ints(2000, 2, 29), Ok(()));
+        assert_eq!(date.set_from_ints(2000, 2, 30), Err("Set date error: day too large"));
+        assert_eq!(date.set_from_ints(2001, 2, 28), Ok(()));
+        assert_eq!(date.set_from_ints(2001, 2, 29), Err("Set date error: day too large"));
+        assert_eq!(date.set_from_ints(2004, 2, 29), Ok(()));
+        assert_eq!(date.set_from_ints(2004, 2, 30), Err("Set date error: day too large"));
+        assert_eq!(date.set_from_ints(2100, 2, 28), Ok(()));
+        assert_eq!(date.set_from_ints(2100, 2, 29), Err("Set date error: day too large"));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use time_tracker::date::Date;
 
 fn main() {
-    let mut date = Date::today();
-    println!("Today's date is {}", date);
-    date.set_day(date.get_day() + 1).expect("Couldn't add a day to today.");
-    println!("Tomorrow's date is {}", date);
+    let today = Date::new_from_today();
+    println!("Today's date is {}", today);
 }
